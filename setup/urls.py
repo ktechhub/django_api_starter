171f0702ce6c2bf0c47af5eb7b082999462893ca
Kalkulus1 <mumunim10@gmail.com>
2022-07-11 22:56:52 +0200

    feat: settings stuff

--- setup/urls.py
+++ setup/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from dj_rest_auth.registration.views import VerifyEmailView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -42,9 +41,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    # path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent')
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
 
 
